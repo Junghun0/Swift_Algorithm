5f18a5ea3ec76222ff889f2bcdb7d4231ac3e47b
parkjunghun <go9018@naver.com>
2019-03-01 19:36:33 +0900

    [2019.03.01]

--- BaekJoon_prac/main.swift
+++ BaekJoon_prac/main.swift
@@ -20,6 +20,7 @@ if let readInput = readInput {
     print(result)
 }*/
 
+
 //10869번
 
 /*var readInput = readLine()
@@ -49,22 +50,24 @@ if let readInput = readInput {
  calc(a: i[0], b: i[1]).forEach { print($0) }
  */
 
+
+
 //10430번
 
-var inputNum = readLine()
+/*var inputNum = readLine()
 
-func calc(num1 : Int, num2: Int, num3: Int) -> [Int]{
+func calc(_ num1 : Int, _ num2 : Int, _ num3: Int) -> [Int]{
     return [(num1 + num2)%num3 , (num1%num3 + num2%num3) % num3 , (num1 * num2) % num3, ((num1 % num3) * (num2 % num3)) % num3]
 }
 
 if let inputNum = inputNum{
     let array = inputNum.components(separatedBy: " ").map { (inputs : String) -> Int in return Int(inputs)! }
-    calc(num1: array[0], num2: array[1], num3: array[2]).forEach { print( $0 ) }
+    //Closure
+    //calc(num1: array[0], num2: array[1], num3: array[2]).forEach { print( $0 ) }
+    calc(array[0], array[1], array[2]).forEach { (array) in
+        print(array)
     }
-    
-
-
-
+}*/
 
 //if let inputNum = inputNum{
 //    let numbers = inputNum.components(separatedBy: " ")
@@ -73,4 +76,116 @@ if let inputNum = inputNum{
 //    //***Int(numbers[0])!
 //    print(numbers)
 //}
+
+
+
+//2558번
+/*var inputNums1 = readLine()
+var inputNums2 = readLine()
+
+if let inputNums1 = inputNums1 , let inputNums2 = inputNums2{
+    let num1 = inputNums1
+    let num2 = inputNums2
+    
+    print(Int(num1)! + Int(num2)!)
+}*/
+
+
+//2839번
+
+/*let inputSugar = readLine()!.components(separatedBy: " ").map { (value : String) -> Int in Int(value)! }
+var counter : Int = 0
+
+var sugar = inputSugar[0]
+
+while(true){
+    
+    if(sugar % 5 == 0){
+        print(sugar/5 + counter)
+        break;
+    }
+    else if(sugar <= 0){
+        print("-1")
+        break;
+    }
+    sugar = sugar - 3
+    counter = counter + 1
+}
+*/
+
+
+//2741번
+
+/*
+let inputNum = readLine()!.components(separatedBy: " ").map { (value: String) -> Int in return Int(value)! }
+var endPoint = inputNum[0]
+
+for result in 1...endPoint{
+    print(result)
+}
+*/
+
+//2438번
+
+/*let inputNum = readLine()!.components(separatedBy: " ").map { (value : String) -> Int in
+    Int(value)!
+}
+
+var starLength = inputNum[0]
+
+Array(1...starLength).forEach { (self) in
+    print(String(repeating: "*", count: self))
+}
+
+Array(1...starLength).reversed().forEach { (self) in
+    print(String(repeating: "*", count: self))
+}*/
+
+//for i in 1...starLength {
+//    var result = ""
+//
+//    for _ in starLength - i + 1 ... starLength{
+//        result = result + "*"
+//    }
+//    print(result)
+//}
+
+
+//8393번
+/*let inputNum = Int(readLine()!)!
+var sum : Int = 0
+for k in 1...inputNum{
+    sum += k
+}
+print(sum)*/
+
+
+//11721번
+let inputString = readLine()!
+
+var arr_String = Array<Character>()
+var result_array = Array<String>()
+var result = ""
+
+for index in inputString.characters.indices{
+    arr_String.append(inputString[index])
+}
+
+if(arr_String.count >= 0){
+    
+    for k in 1...arr_String.count{
+        result = result + String(arr_String[k-1])
+        if(k % 10 == 0){
+            result_array.append(result)
+            print(result)
+            result = ""
+            
+        }else if(arr_String.count - k < 10){
+            result_array.append(result)
+        }
+    }
+    print(result)
+}
+
+
 
